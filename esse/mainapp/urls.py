6b490a29566cec02b8c2cd954bcd3b4b1d2e1e63
Ivan <alexeevivan92@gmail.com>
2021-02-26 13:50:48 +0300

    Update urls.py

--- esse/mainapp/urls.py
+++ esse/mainapp/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
 from . import views
-from .views import test_view, ProductDetailView, CategoryDetailView, CartView, info_about, info_pricing, info_library, info_books_list
+from .views import test_view, ProductDetailView, CategoryDetailView, CartView, info_about, info_pricing, info_library, info_books_list, info_biography
 
 # include base.html as a instrument to render the main.html file
 urlpatterns = [
     path('', test_view, name='base'),
     path('<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
-    path('category/<str:slug>/', CategoryDetailView.as_view(), name='category_detail'),   
+    path('library/<str:slug>/', CategoryDetailView.as_view(), name='category_detail'),   
     path('about', views.info_about, name='about'),
     path('pricing', views.info_pricing, name='pricing'),
     path('library', views.info_library, name='library'),
     path('library/books_list', views.info_books_list, name='books_list'),
+    path('library/biography', views.info_biography, name='biography'),
+    path('library/economics', views.info_economics, name='economics'),
+    path('library/history', views.info_history, name='history'),
+    path('library/medicine', views.info_medicine, name='medicine'),
+    path('library/novel', views.info_novel, name='novel'),
     path('cart/', CartView.as_view(), name='cart')
 ]
 
