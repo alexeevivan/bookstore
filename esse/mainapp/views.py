54671b09abb12a6199e2f6d2eef4be008f8115ad
Ivan <alexeevivan92@gmail.com>
2021-03-01 13:35:10 +0300

    Update views.py

--- esse/mainapp/views.py
+++ esse/mainapp/views.py
@@ -109,6 +109,21 @@ class CategoryDetailView(CartMixin, CategoryDetailMixin, DetailView):
         return context
 
 
+class AddToCartView(View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer, in_order=False)
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product = CartProduct.objects.create(
+            user=cart.owner, cart=cart, content_type=content_type, object_id=product.id, final_price=product.price
+        )
+        cart.products.add(cart_product)
+        return HttpResponseRedirect('/cart/')
+
+
 class CartView(CartMixin, View):
 
     def get(self, request, *args, **kwargs):
