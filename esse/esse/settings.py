c9b2496d807546a8d7882e48553a1a5c31c0a447
Ivan <alexeevivan92@gmail.com>
2021-03-03 03:33:50 +0300

    Update settings.py

--- esse/esse/settings.py
+++ esse/esse/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
