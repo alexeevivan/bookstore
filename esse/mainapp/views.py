269ae1527664ef604833775d855beaffda0c4831
Ivan <alexeevivan92@gmail.com>
2021-03-10 03:04:34 +0300

    Update views.py

--- esse/mainapp/views.py
+++ esse/mainapp/views.py
@@ -1,14 +1,16 @@
 from django.db import transaction
 from django.db import reset_queries
+from django.conf import settings
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.views.generic import DetailView, View
 from django.http import HttpResponseRedirect
 
-from .models import Biography, Economics, History, Medicine, Novel, Category, LatestProducts, Customer, Cart, CartProduct
+from .models import Biography, Economics, History, Medicine, Novel, Category, LatestProducts, Customer, Cart, CartProduct, Order
 from .mixins import CategoryDetailMixin, CartMixin
-from .forms import OrderForm
+from .forms import OrderForm, LoginForm, RegistrationForm
 from .utils import recalc_cart
 
 
@@ -120,7 +122,7 @@ class AddToCartView(CartMixin, View):
             self.cart.products.add(cart_product)
         #self.cart.save()
         recalc_cart(self.cart)
-        messages.add_message(request, messages.INFO, "Product added successfully")
+        #messages.add_message(request, messages.INFO, "Product added successfully")
         return HttpResponseRedirect('/cart/')
 
 
@@ -206,3 +208,86 @@ class MakeOrderView(CartMixin, View):
             messages.add_message(request, messages.INFO, 'Thanks for order! Our Manager will call you in 10 minutes approximately.')
             return HttpResponseRedirect('/')
         return HttpResponseRedirect('confirmation')
+
+
+class LoginView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        categories = Category.objects.all()
+        context = {
+            'form': form,
+            'categories': categories,
+            'cart': self.cart
+        }
+        return render(request, 'login.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        context = {
+            'form': form,
+            'cart': self.cart
+        }
+        return render(request, 'login.html', context)
+
+
+class RegistrationView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST or None)
+        categories = Category.objects.all()
+        context = {
+            'form': form,
+            'categories': categories,
+            'cart': self.cart
+        }
+        return render(request, 'registration.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.username = form.cleaned_data['username']
+            new_user.email = form.cleaned_data['email']
+            new_user.first_name = form.cleaned_data['first_name']
+            new_user.last_name = form.cleaned_data['last_name']
+            new_user.save()
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            Customer.objects.create(
+                user=new_user,
+                phone=form.cleaned_data['phone'],
+                address=form.cleaned_data['address']
+            )
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            login(request, user)
+            return HttpResponseRedirect('/')
+        context = {
+            'form': form,
+            'cart': self.cart
+            }
+        return render(request, 'registration.html', context)
+
+
+class ProfileView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        orders = Order.objects.filter(customer=customer).order_by('-created_at')
+        categories = Category.objects.all()
+        return render(
+            request,
+            'profile.html',
+            {
+                'orders': orders,
+                'cart': self.cart,
+                'categories':categories
+            }
+        )
