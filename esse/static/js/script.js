7619c2860155287b834dd4771b19386f2f923818
Ivan <alexeevivan92@gmail.com>
2021-03-15 23:31:32 +0300

    Create script.js

---
+++ esse/static/js/script.js
@@ -0,0 +1,9 @@
+// typing animation script
+var typed = new Typed(".typing", {
+    strings: [
+        "project", "enterprise", "opportunity"
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+})
